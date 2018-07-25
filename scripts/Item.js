7d1d949645574561d338ab5b54671525375fc1f7
David Graves <davidgraves831@gmail.com>
2018-07-25 10:24:05 -0500

    finished step 1

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,20 +1,22 @@
+'use strict';
+
 const Item = (function () {
-	return {
-        validateName,
-        create
-    };
+  return {
+    validateName,
+    create
+  };
 } () );
 
 function validateName(name) {
-	if (!name) {
-		throw new TypeError('Name does not exist');
-	}
+  if (!name) {
+    throw new TypeError('Name does not exist');
+  }
 }
 
 function create(name) {
-    return {
-        id : cuid(),
-        name : name,
-        checked: false
-    }
+  return {
+    id : cuid(),
+    name : name,
+    checked: false
+  }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
-/* global shoppingList, cuid, $ */
-
+/* global shoppingList, cuid, api, $ */
+'use strict';
 // eslint-disable-next-line no-unused-vars
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
+
+api.getItems(function(data) {
+  console.log(data);
+});
+
+console.log(api.BASE_URL);
